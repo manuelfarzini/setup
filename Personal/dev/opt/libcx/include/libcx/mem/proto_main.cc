94b1d54aef63ac7dedc4830821aad1796584587e
Manuel Farzini <manuel.farzini@gmail.com>
2026-03-25 18:12:15 +0100

    Periodic backup

--- Personal/dev/opt/libcx/include/libcx/mem/proto_main.cc
+++ Personal/dev/opt/libcx/include/libcx/mem/proto_main.cc
@@ -1,9 +1,14 @@
-#include <libcx/mem/multi.hh>
+// #include <libcx/mem/multi.hh>
+#include "stdio.h"
+#include "libcx/mem/const.hh"
 
 int main() {
-  puts("\nmemcopy_multi_1");
-  test_memcopy_multi_1();
+  // puts("\nmemcopy_multi_1");
+  // test_memcopy_multi_1();
 
-  puts("\nbind_multi_1");
-  test_bind_multi_1();
+  // puts("\nbind_multi_1");
+  // test_bind_multi_1();
+
+  puts("\ntest_const_1");
+  test_const_1();
 }
