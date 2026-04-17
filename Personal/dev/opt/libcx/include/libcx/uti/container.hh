f339461a6ed09d3784f2972abf8ef5dbb9041a9d
Manuel Farzini <manuel.farzini@gmail.com>
2026-04-17 17:41:31 +0200

    Periodic update.

--- Personal/dev/opt/libcx/include/libcx/uti/container.hh
+++ Personal/dev/opt/libcx/include/libcx/uti/container.hh
@@ -8,48 +8,62 @@
 
 namespace cx {
 
-//−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+// −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 // Type erasure
 
 /// Predicate `true` if `T` is a container with a raw `storage`.
-template<typename C> concept CErasedContainer = requires(C c) {
-  requires is_raw_array<decltype(c.storage)>;
-  requires same_as<
-      cx::rm_extent<rm_ref<decltype(c.storage)>>,
-      u8
-  >;
+template<typename C>
+concept SomeErasedContainer = requires(C c) {
+    requires is_raw_array<decltype(c.storage)>;
+    requires same_as<
+        cx::rm_extent<rm_ref<decltype(c.storage)>>, u8
+    >;
 };
 
 /// XXX:
 template<typename T, Requires(!is_func<T>, !is_void<T>)>
 nodisc onedef cexpr proc launder(T* p) noexce -> T*
 {
-  return __builtin_launder(p);
+    return __builtin_launder(p);
 }
 
 /// Returns: a `T` pointer to the raw memory of an `ErasedContainer` `c`
 /// Note: used to construct the element(s) in `c`.
-template<CErasedContainer C>
-finline onedef cexpr proc get_place(C& c) noexce -> uti::ElemIn<C>*
+// template <CErasedContainer C>
+// finline onedef cexpr proc get_place(C& c) noexce -> uti::ElemIn<C>*
+// {
+//   return c.storage;
+// }
+
+meta <SomeErasedContainer C> 
+finline consfn(get_place, C& c) -> uti::ElemIn<C>*
 {
-  return c.storage;
+    return c.storage;
 }
 
 /// Returns: a `T` pointer to the object in an `ErasedContainer` `c`
 /// Note: used to get the fully constructed object in `c`.
-template<CErasedContainer C>
-finline onedef cexpr proc get_object(C& c) noexce -> uti::ElemIn<C>*
+// template<SomeErasedContainer C>
+// finline onedef cexpr proc get_object(C& c) noexce -> uti::ElemIn<C>*
+// {
+//     return launder(get_place<C>(c));
+// }
+
+meta <SomeErasedContainer C>
+finline consfn(get_object, C& c) -> uti::ElemIn<C>*
 {
   return launder(get_place<C>(c));
 }
 
-//−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-// Key type
+// −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+//  Key type
 
 template<typename T> concept HasKeyIn = requires { typename T::KeyType; };
-template<typename T, typename = void> struct ___KeyOrSelf { using Type = T; };
+template<typename T, typename = void> struct ___KeyOrSelf {
+    using Type = T;
+};
 template<typename T> struct ___KeyOrSelf<T, Void<typename T::KeyType>> {
-  using Type = typename T::KeyType;
+    using Type = typename T::KeyType;
 };
 /// Extract the key type from a container. If the `ElemType` in the container
 /// does not have a `KeyType`, it is set as the `ElemType` itself.
@@ -65,5 +79,5 @@ template<typename T> struct ___KeyOrSelf<T, Void<typename T::KeyType>> {
 /// ```
 template<typename T> using KeyOrSelf = typename ___KeyOrSelf<T>::Type;
 
-}       // namespace cx
+}  // namespace cx
 #endif  // CX___UTILS_CONTAINER_HH
