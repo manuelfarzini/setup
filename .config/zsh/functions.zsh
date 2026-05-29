35ca5ef4a2a8d697131b1e7c026d5d94c8b5aa2a
Manuel Farzini <manuel.farzini@gmail.com>
2026-05-29 10:07:55 +0200

    Periodic update.

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -7,3 +7,21 @@ function y() {
 	fi
 	rm -f -- "$tmp"
 }
+
+# Update dotfiles and configurations
+setup_update() {
+    msg="Periodic update."
+    nvim="$HOME/.config/nvim"
+
+    setup_git() {
+        git --git-dir="$HOME/.setup" --work-tree="$HOME" "$@"
+    }
+
+    git -C "$nvim" add .
+    git -C "$nvim" diff --cached --quiet || git -C "$nvim" commit -m "$msg"
+    git -C "$nvim" push
+
+    setup_git add -u
+    setup_git diff --cached --quiet || setup_git commit -m "$msg"
+    setup_git push
+}
