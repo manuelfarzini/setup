4d89077725cf646ff01f1a730df46d8fe735cd24
Manuel Farzini <manuel.farzini@gmail.com>
2026-05-29 10:15:34 +0200

    Minor fix.

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,5 +1,6 @@
 # Yazi navigation
-function y() {
+function y() 
+{
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
@@ -9,18 +10,14 @@ function y() {
 }
 
 # Update dotfiles and configurations
-setup_update() {
-    msg="Periodic update."
-    nvim="$HOME/.config/nvim"
-
-    setup_git() {
-        git --git-dir="$HOME/.setup" --work-tree="$HOME" "$@"
-    }
-
+setup_update()
+{
+    local msg="${1:-Periodic update.}"
+    local nvim="$HOME/.config/nvim"
+    setup_git() { git --git-dir="$HOME/.setup" --work-tree="$HOME" "$@" }
     git -C "$nvim" add .
     git -C "$nvim" diff --cached --quiet || git -C "$nvim" commit -m "$msg"
     git -C "$nvim" push
-
     setup_git add -u
     setup_git diff --cached --quiet || setup_git commit -m "$msg"
     setup_git push
