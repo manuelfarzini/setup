8a8c9fc75ea04d548f31e9beb99a1628ed3bebdc
Manuel Farzini <manuel.farzini@gmail.com>
2026-06-08 14:32:07 +0200

    Periodic update.

--- Personal/dev/script/faust_compile_mojo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NAME=$1
-ARCH=$2
-SRC=$3
-
-/Users/manuelfarzini/Personal/dev/repo/faust/build/bin/faust -lang mojo -I dsp -double -i -a $ARCH.mojo $SRC/$NAME.dsp -o $NAME.mojo && nvim $NAME.mojo
-
